2362007347b4b2b8e11ec2b41b970196f12cf9d5
emagrorrom <373407641@qq.com>
2015-11-21 20:19:30 +0800

    Exam Finish
    This is the first version and if I have time,I will try to recoding all this
    problem!

--- src/calculate-third.js
+++ src/calculate-third.js
@@ -10,7 +10,64 @@ function even_group_calculate_average(arr) {
     }
   }
   if (tempArray2.length == 0) {
+    //Test 2
     tempArray2.push(0);
+    return tempArray2;
+  }else {
+    var hasOneTwo = false;
+    for (var i = 0; i < tempArray2.length; i++) {
+      if (tempArray2[i] < 100) {
+        hasOneTwo = true;
+      }
+    }
+    if (!hasOneTwo) {
+      //Test 3
+      var sum = 0;
+      for (var i = 0; i < tempArray2.length; i++) {
+        sum = sum + tempArray2[i];
+      }
+      var avg = sum / tempArray2.length;
+      return [avg];
+    }else {
+      //Test 1
+      var result = [];
+      var k = 10;
+      while (true) {
+        if (smallThanKLevel(tempArray2,k)) {
+          //Main Test 1
+          var l = k / 10;
+          while (l >= 1) {
+            var sum = 0;
+            var count = 0;
+            for (var i = 0; i < tempArray2.length; i++) {
+              if (tempArray2[i] >= l && tempArray2[i] < (10 * l)) {
+                sum += tempArray2[i];
+                count++;
+              }
+            }
+            if (count != 0) {
+              result.push(sum / count);
+            }
+            l = l / 10;
+          }
+          for (var i = 0; i < (result.length-1) / 2; i++) {
+            var temp = result[i];
+            result[i] = result[result.length-1-i];
+            result[result.length-1-i] = temp;
+          }
+          return result;
+        }
+        k = k * 10;
+      }
+    }
   }
+}
 
+function smallThanKLevel(arr,k){
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] > k) {
+      return false;
+    }
+  }
+  return true;
 }
